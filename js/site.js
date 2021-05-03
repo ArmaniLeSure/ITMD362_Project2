5e6ec821ecd1894f48d70eb10302818f26dd684f
Ryan Henderson <ryan.ev.henderson@gmail.com>
2021-05-03 11:39:55 -0500

    Fix spacing in js

--- js/site.js
+++ js/site.js
@@ -1,19 +1,18 @@
-jQuery(function($){
-	$('html').removeClass('nojs');
-	$('html').addClass('hasjs');
+jQuery(function($) {
+    $('html').removeClass('nojs');
+    $('html').addClass('hasjs');
 });
 
 form.onsubmit = submit;
 
 function validateForm() {
-	var a = document.forms["CONTACT"]["FName"].value;
-	var b = document.forms["CONTACT"]["LName"].value;
-	var c = document.forms["CONTACT"]["Email"].value;
-	var d = document.forms["CONTACT"]["Reason"].value;
-	if ((a == "") || (b == "")|| (c == "")){
-		alert("Please fill in all sections.");
-	}
-	else{
-		alert("Welcome to...!");
-	}
+    var a = document.forms["CONTACT"]["FName"].value;
+    var b = document.forms["CONTACT"]["LName"].value;
+    var c = document.forms["CONTACT"]["Email"].value;
+    var d = document.forms["CONTACT"]["Reason"].value;
+    if ((a == "") || (b == "") || (c == "")) {
+        alert("Please fill in all sections.");
+    } else {
+        alert("Welcome to...!");
+    }
 }
